f8b60aac987db7e3bfe5394d0cdfbca629768dd4
kewou.durand <kewoudurand@gmail.com>
2023-10-08 05:20:21 +0200

    fusion de deux branche avec modification

--- src/main/java/be/eafcuccle/kewou_durand/Jeux.java
+++ src/main/java/be/eafcuccle/kewou_durand/Jeux.java
@@ -3,12 +3,13 @@ package be.eafcuccle.kewou_durand;
 public class Jeux {
     public static void main(String[] args) {
         int nombre1 = 5;
+        int nombre0 = 5;
         int nombre2 = 7;
-        int somme = veuxjouter(nombre1, nombre2);
+        int somme = veuxjouter(nombre1, nombre2 ,nombre0 );
         System.out.println("La somme de " + nombre1 + " et " + nombre2 + " est : " + somme);
     }
 
-    public static int veuxjouter(int a, int b) {
+    public static int veuxjouter(int a, int b,int c) {
         return a + b;
     }
 }

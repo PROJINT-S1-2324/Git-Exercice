54967b75f58198265578737fab5854e10985b53d
kewou.durand <kewoudurand@gmail.com>
2023-10-09 12:09:22 +0200

    3eme commit

--- src/main/java/be/eafcuccle/kewou_durand/Jeux.java
+++ src/main/java/be/eafcuccle/kewou_durand/Jeux.java
@@ -5,8 +5,9 @@ public class Jeux {
         int nombre1 = 5;
         int nombre0 = 5;
         int nombre2 = 7;
+
         int somme = veuxjouter(nombre1, nombre2 ,nombre0 );
-        System.out.println("La somme de " + nombre1 + " et " + nombre2 + " est : " + somme);
+        System.out.println(" La somme de " + nombre1  + " ; " +  nombre1 + " et " + nombre2 + " est : " + somme);
     }
 
     public static int veuxjouter(int a, int b,int c) {
